ea7007fb8ebc0e22127a55ab9b62eaba03db431a
jpsymfony <jpsymfony@free.fr>
2016-03-11 00:51:06 +0100

    translations updated

--- src/App/PortalBundle/Form/Type/ImageType.php
+++ src/App/PortalBundle/Form/Type/ImageType.php
@@ -11,7 +11,7 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('file', 'file')
+            ->add('file', 'file', array('label' => 'film.file'))
         ;
     }
 
